e5f4c8f6fc86c6cacfa3220b043e8dc98443a477
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-06-27 17:32:46 +0200

    refactor: better indent

--- src/decorators/multithreaded.py
+++ src/decorators/multithreaded.py
@@ -2,13 +2,14 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import wraps
 
+
 def multithreaded(fct):
     """Decorators to put on a function to execute it on multiple thread.
     It is used to speed up long I/O process such as fetching data from API
     or reading files."""
     @wraps(fct)
     def wrapper(lst):
-        max_workers=32
+        max_workers = 32
         if len(lst) < max_workers:
             max_workers = len(lst)
         if max_workers == 1:
